88f93a908ea103724343abefb5aba01b21b119b3
Blake Rainwater <rainwaterblake@gmail.com>
2015-05-30 20:06:34 -0400

    Added some test data and created a setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'rivercrypt',
+    packages = ['rivercrypt'],
+    version = 'v0.3.1',
+    description = 'Unix style utility for asymmetrically encrypting and decrypting files in a shell pipeline',
+    author = 'Blake Rainwater',
+    author_email = 'rainwaterblake@gmail.com',
+    url = 'https://github.com/brainwater/rivercrypt',
+    download_url = 'https://github.com/brainwater/rivercrypt/tarball/v0.3.1',
+    keywords = ['encryption'],
+    classifiers = [],
+    install_requires = ['libnacl', 'simpleubjson'],
+)
